e6f3eed29ba075f9578aedd062b108f553ed2d33
unknown <raziyebayat75@gmail.com>
2024-04-28 15:42:13 +0330

    acordion and footer

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  const close_accordion = document.getElementById("close-accordion");
+
   $("#eng").click(function () {
     $("#eng-icon").toggleClass("icon-rotate");
     $("#dropdown-eng").toggleClass("show-dropdown");
@@ -8,5 +10,65 @@ $(document).ready(function () {
   });
   $(".menu-bar").click(function () {
     $(".menu-bar-hamberger").toggleClass("show-dropdown");
+  });
+
+  document.querySelectorAll(".thumbnail").forEach((item) => {
+    item.addEventListener("click", function () {
+      document.querySelectorAll(".thumbnail").forEach((innerItem) => {
+        innerItem.classList.remove("active");
+      });
+      item.classList.add("active");
+      document.getElementById("main-img").src = item.src;
+    });
+  });
+
+  $("#accordion-custom1").click(function () {
+    $("#main-accordion1").toggleClass("show");
+    $("#icon-accordion1").toggleClass("icon-rotate");
+  });
+
+  $("#close-accordion1").click(function () {
+    $("#main-accordion1").toggleClass("show");
+    $("#icon-accordion1").toggleClass("icon-rotate");
+  });
+
+  $("#accordion-custom2").click(function () {
+    $("#main-accordion2").toggleClass("show");
+    $("#icon-accordion2").toggleClass("icon-rotate");
+  });
+
+  $("#close-accordion2").click(function () {
+    $("#main-accordion2").toggleClass("show");
+    $("#icon-accordion2").toggleClass("icon-rotate");
+  });
+
+  $("#accordion-custom3").click(function () {
+    $("#main-accordion3").toggleClass("show");
+    $("#icon-accordion3").toggleClass("icon-rotate");
+  });
+
+  $("#close-accordion3").click(function () {
+    $("#main-accordion3").toggleClass("show");
+    $("#icon-accordion3").toggleClass("icon-rotate");
+  });
+
+  $("#accordion-custom4").click(function () {
+    $("#main-accordion4").toggleClass("show");
+    $("#icon-accordion4").toggleClass("icon-rotate");
+  });
+
+  $("#close-accordion4").click(function () {
+    $("#main-accordion4").toggleClass("show");
+    $("#icon-accordion4").toggleClass("icon-rotate");
+  });
+
+  $("#accordion-custom5").click(function () {
+    $("#main-accordion5").toggleClass("show");
+    $("#icon-accordion5").toggleClass("icon-rotate");
+  });
+
+  $("#close-accordion5").click(function () {
+    $("#main-accordion5").toggleClass("show");
+    $("#icon-accordion5").toggleClass("icon-rotate");
   });
 });
